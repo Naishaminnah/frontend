36ecd796eac543c54c8db22cf563abb2a17b9ba4
Naishaminnah <155439876+Naishaminnah@users.noreply.github.com>
2024-04-14 18:05:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+}  from "react-router-dom";
+import Home from './Pages/Home';
+import AddBlog from './Pages/AddBlog';
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <Navbar /> 
+     <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/add_blog' element={<AddBlog/>}/>
+
+
+     </Routes>
+     
+     
+     
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
