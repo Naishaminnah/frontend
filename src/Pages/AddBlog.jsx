8518b0382d12202ee494e2b08aa3114c9ffd62ce
Naishaminnah <155439876+Naishaminnah@users.noreply.github.com>
2024-04-14 18:15:48 +0530

    Add files via upload

---
+++ src/Pages/AddBlog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BlogForm from '../Components/BlogForm/BlogForm';
+
+const AddBlog = () => {
+  return (
+    <div>
+    <BlogForm/>
+ </div>
+  )
+}
+export default AddBlog;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Myblog from '../Components/Myblog/Myblog'
+
+const Home = () => {
+  return (
+    <div>
+
+<Myblog />
+    </div>
+  )
+}
+export default Home
